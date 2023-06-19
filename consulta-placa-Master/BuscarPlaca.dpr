89ab10d1552290e8276480289a264a1741e75504
Guilherme Fernando Pires <75048254+guilhermepires06@users.noreply.github.com>
2023-06-19 15:34:28 -0300

    Add files via upload

---
+++ consulta-placa-Master/BuscarPlaca.dpr
@@ -0,0 +1,17 @@
+program BuscarPlaca;
+
+uses
+  Vcl.Forms,
+  uMain in 'view\uMain.pas' {frmMain},
+  Vcl.Themes,
+  Vcl.Styles;
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Sky');
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
